602210bdd535d0c90cc1bc3ce156bbd42798ca8b
scamlet <ismailbozan90@gmail.com>
2024-07-06 13:53:35 +0300

    First commit

---
+++ src/test/java/com/raidtool/signup/SignupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raidtool.signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
